9ebf181659ebba4f5cc517c1aa4a6341777ffc9b
darshan <darshankhot@protonmail.com>
2019-10-12 20:35:32 +0530

    added p7_3

---
+++ p7/p7_3/perform.java
@@ -0,0 +1,49 @@
+class Producer extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			System.out.println("Producing the product...");
+			// sleep(1000);
+			System.out.println("Finished Production...");
+			// sleep(1000);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+class Consumer extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			System.out.println("Purchasing the product...");
+			// sleep(1000);
+			System.out.println("Finished Purchase...");
+			// sleep(1000);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+
+class Perform
+{
+	public static void main(String args[])
+	{
+		Producer p = new Producer();
+		Consumer c = new Consumer();
+		for(int i = 0; i<10; i++)
+		{
+			p.run();
+			c.run();
+		}
+		
+	}
+}
